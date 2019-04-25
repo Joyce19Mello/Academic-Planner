9b44c7d44a8801184b66f820171c94c81915a4a8
joyce.rodrigues <joycemelo.lemaf@gmail.com>
2019-04-25 14:32:37 -0300

    criação das entidades finalizadas

--- backend/app/models/Publicacao.java
+++ backend/app/models/Publicacao.java
@@ -1,4 +1,33 @@
 package models;
 
-public class Publicacao {
+import play.db.jpa.GenericModel;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Publicacao extends GenericModel {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name="nome_publicacao")
+    public String nomeTituloPublicacao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_projeto", referencedColumnName="id")
+    public Projeto projeto;
+
+    @Column(name="ano_publicacao")
+    public Date anoPublicacao;
+
+    @Column(name="resumo_publicacao")
+    public String resumoPublicacao;
+
+    @Column(name = "arquivo_digital")
+    public String arquivoDigitalBibtex;
+
+
 }
